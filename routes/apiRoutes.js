f320545456b03a6794151e803cde40e5586cab66
bluninjadragon <jwhbootcamp@gmail.com>
2021-08-28 21:21:33 -0500

    updated apiRoute post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
+const path = require("path");
 
 router.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../db/db.json"));
   });
   
-  router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../db/db.json"));
+  router.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uniqid();
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const parseData = JSON.parse(data);
+        console.log(JSON.parse(data));
+        parseData.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(parseData), err => {
+            if (err) throw err;
+            res.json(parseData)
+        })
+    })
   });
